5054335cbf0933452c05c435c181227d1eff4f5a
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-04-25 23:10:55 +0530

    some value min and max fixes in fees.php

--- fees.php
+++ fees.php
@@ -7,25 +7,27 @@ include('db.php');
 // <? get variable adress 
 
 $scholar_no = "";
-if(isset($_GET['scholar_no'])){
+if (isset($_GET['scholar_no'])) {
     $scholar_no = $_GET['scholar_no'];
 }
 
-$student_name ="";
-if(isset($_GET['student_name'])) {
+$student_name = "";
+if (isset($_GET['student_name'])) {
     $student_name = $_GET['student_name'];
 }
 
-if(isset($_GET['scholar_no'])) {
+if (isset($_GET['scholar_no'])) {
     $scholar = mysqli_real_escape_string($conn, $_GET['scholar_no']);
     // Table name correctly matched: newadmission
     $query = "SELECT * FROM newadmissions WHERE scholar_no = '$scholar'";
     $res = mysqli_query($conn, $query);
     $data = mysqli_fetch_assoc($res);
 
-    if(!$data) { die("<h2 style='text-align:center; padding-top:50px;'>Student Not Found!</h2>"); }
+    if (!$data) {
+        die("<h2 style='text-align:center; padding-top:50px;'>Student Not Found!</h2>");
+    }
 } else {
-    header("Location:dash.php"); 
+    header("Location:dash.php");
     exit();
 }
 ?>
@@ -38,44 +40,43 @@ if(isset($_GET['scholar_no'])) {
     <form action="acc.php" method="post">
 
         <label>Scholar No </label>
-        <input type="number" value="<?=  $scholar_no ?>"
-        name="scholar_no" placeholder="auto generated" readonly required>
+        <input type="number" value="<?= $scholar_no ?>"
+            name="scholar_no" placeholder="auto generated" readonly required>
 
         <label>Student Name</label>
         <input value="<?= $data['student_name'] ?>" readonly required>
         <label>Class</label>
-   <input value="<?= $data['student_class'] ?>" name="student_class" readonly>
+        <input value="<?= $data['student_class'] ?>" name="student_class" readonly>
         <label>Installment No </label>
-        <input type="tel" name="installment_no" placeholder="enter installment no 1,2 etc " 
-        onchange="if(this.value < 0) this.value = 0;"
-        required>
+        <input type="number" name="installment_no" placeholder="enter installment no 1,2 etc "
+            min='1'
+            required>
         <label>Date</label>
-        <input type="date" name="date" placeholder="dd-mm-yyyy"required>
-                <label>Total Fee Amount</label>
-                <input type="number" value="<?= $data['total_standard_fees'] ?>" name="total_standard_fees" readonly>
+        <input type="date" name="date" placeholder="dd-mm-yyyy" required>
+        <label>Total Fee Amount</label>
+        <input type="number" value="<?= $data['total_standard_fees'] ?>" name="total_standard_fees" readonly>
         <label>Diposit amount </label>
-        <input type="tel" name="diposite_amt" placeholder="enter diposite amount here "required
-         onchange="if(this.value < 0) this.value = 0;">
+        <input type="number" min="1" name="diposite_amt" placeholder="enter diposite amount here " required>
         <label>Discount </label>
-        <input type="tel" name="discount_amt" placeholder="enter diposite amount here "
-         onchange="if(this.value < 0) this.value = 0;">
-              <label>Recieved by  </label>
-        <input type="text" name="recieved" placeholder="teacher/faculty "required>    
+        <input type="number" min='0' name="discount_amt" placeholder="enter diposite amount here "
+            onchange="if(this.value < 0) this.value = 0;">
+        <label>Recieved by </label>
+        <input type="text" name="recieved" placeholder="teacher/faculty " required>
         <label>Mode of payment </label>
         <!-- <input type="text" name="mode" placeholder="online/cash "required -->
         <select class="select">
             <option>Cash</option>
             <option>Online</option>
-         </select>
-        
+        </select>
+
         <label>Remarks</label>
         <input type="text" name="remarks" placeholder="remarks etc ">
-    <p >***NOTE***<br> 
-        1 : All feilds are filled carefully and recheck again <br>
-              2 : Check correctly STUDENT NAME and its SCHOLAR NO and reconfirmed before generate reciept
-</p>
+        <p>***NOTE***<br>
+            1 : All feilds are filled carefully and recheck again <br>
+            2 : Check correctly STUDENT NAME and its SCHOLAR NO and reconfirmed before generate reciept
+        </p>
 
-<button class="btn" >Generate reciept </button>
+        <button class="btn">Generate reciept </button>
     </form>
 
 
@@ -83,6 +84,5 @@ if(isset($_GET['scholar_no'])) {
 
 </div>
 <script src="fees.js">
-
     //        <label>Reciept No </label>
-        // <input type="number" value="auto gnerated" name="reciept_no" readonly style="background: #eee;">
+    // <input type="number" value="auto gnerated" name="reciept_no" readonly style="background: #eee;">
