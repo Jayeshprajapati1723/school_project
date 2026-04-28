b96cbde3da984ec77d1304c84db567ac495afe19
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-04-29 00:13:34 +0530

    theme page add

--- theme.js
+++ theme.js
@@ -2,12 +2,12 @@ let theme = document.querySelector('.thbtn') ;
 let istrue = 1 ;
 theme.addEventListener("click",()=> {  
 if(istrue==1) {
-    document.body.style.backgroundColor = "white" ;
-    document.body.style.color = "black" ;
+    document.body.style.backgroundColor = "black" ;
+    document.body.style.color = "blue" ;
     istrue =0 
 } else {
-    document.body.style.color ="white"
-    document.body.style.backgroundColor = "black" ;
+        document.body.style.backgroundColor = "white" ;
+    document.body.style.color ="black"
     istrue=1 ;
 }
 }
